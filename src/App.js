7472a6631afb2337a08b57409594200971329fa3
Suraj Verma <surajverma2587@gmail.com>
2019-11-03 19:48:41 +0000

    wrap app within a container and add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 import SearchWidget from './components/SearchWidget'
 
 const App = () => (
-  <SearchWidget />
+  <div className="container">
+    <h1>React Search Bar App</h1>
+    <div className="links">
+      <a href="https://github.com/surajverma2587/react-search-app" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+    </div>
+    <SearchWidget />
+  </div>
 )
 
 export default App
