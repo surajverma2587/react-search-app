adef14ee4a20c6c9c7e9988b11d25dc027dd8fb8
Suraj Verma <surajverma2587@gmail.com>
2019-11-03 14:08:54 +0000

    add props tests for SearchResults component

--- src/tests/components/SearchResults/index.test.js
+++ src/tests/components/SearchResults/index.test.js
@@ -3,17 +3,25 @@ import { shallow } from 'enzyme'
 
 import SearchResults from '../../../components/SearchResults'
 
-const render = (results) => shallow(<SearchResults results={results} />)
+const render = (props) => shallow(<SearchResults {...props} />)
 
 describe('Search Results', () => {
-  test('should render the SearchResults component', () => {
-    expect(render(['foo', 'bar']).find('.c-search-results').exists()).toEqual(true)
+  test('should render the SearchResults component when results are not empty and showResults is TRUE', () => {
+    const props = { results: ['foo', 'bar'], showResults: true }
+    expect(render(props).find('.c-search-results').exists()).toEqual(true)
+  })
+
+  test('should not render the SearchResults component with default props', () => {
+    expect(render().find('.c-search-results').exists()).toEqual(false)
   })
 
   test('should render the SearchResults component with the expected search result items', () => {
-    const results = ['foo', 'bar', 'biz', 'baz']
-    const items = render(results).find('.c-search-results__item')
-    expect(items.length).toEqual(4)
-    expect(items.map(item => item.text())).toEqual(results)
+    const props = {
+      results: ['foo', 'bar', 'biz', 'baz'],
+      showResults: true,
+    }
+    const items = render(props).find('.c-search-results__item')
+    expect(items.length).toEqual(props.results.length)
+    expect(items.map(item => item.text())).toEqual(props.results)
   })
 })
